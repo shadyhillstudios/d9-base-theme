297d88f1a6bc4e111e4bf039dc065752e2ecec1f
Irwin Sentilles <irwin@shadyhillstudios.com>
2021-12-03 18:26:50 +0100

    changing gulp file to have a build css for post composer

--- gulpfile.js
+++ gulpfile.js
@@ -18,3 +18,12 @@ gulp.task('watch', function(){
     livereload.listen();
     gulp.watch('./scss/**/*.scss', gulp.series('scss'));
 });
+
+gulp.task('build-css', function () {
+  return gulp.src(['./scss/style.scss', './scss/ckeditor.scss'])
+    .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'));
+});
